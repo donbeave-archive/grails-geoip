4d959541b0bbb953770819cad2be310a810641da
BeaVe <donbeave@gmail.com>
2013-11-21 15:55:21 +0800

    Init 0.3-SNAPSHOT version.

---
+++ grails-app/services/org/grails/plugin/geoip/GeoIpService.groovy
@@ -0,0 +1,47 @@
+/* Copyright 2009 Lightway Software SRL. All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.grails.plugin.geoip
+
+/**
+ * Service for determining a geographical location based on an IP. 
+ *
+ * @author Radu Andrei Tanasa
+ */
+public class GeoIpService {
+
+  def geoLookupService
+
+  def getLocation(def ip) {
+    geoLookupService.getLocation(ip)
+  }
+
+  def getIpAddress(request) {
+    def ipAddress = request.getHeader('X-Real-IP')
+
+    if (!ipAddress)
+      ipAddress = request.getHeader('Client-IP')
+
+    if (!ipAddress)
+      ipAddress = request.getHeader('X-Forwarded-For')
+
+    if (!ipAddress)
+      ipAddress = request.remoteAddr
+
+    return ipAddress
+  }
+
+}
---
+++ scripts/InstallGeoData.groovy
@@ -0,0 +1,54 @@
+/* Copyright 2013 the original author or authors.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+includeTargets << grailsScript("_GrailsEvents")
+includeTargets << grailsScript('_GrailsBootstrap')
+
+geoPath = '/data/maxmind'
+geoLiteSources = "${basedir}/web-app${geoPath}"
+appDir = "$basedir/grails-app"
+
+target(installGeoLite: 'Downloads GeoLite from dev.maxmind.com') {
+  event('StatusUpdate', ['Downloading GeoLite'])
+
+  mkdir(dir: "${geoLiteSources}")
+
+  delete(file: "${geoLiteSources}/GeoLiteCity.dat")
+
+  get(dest: "${geoLiteSources}/GeoLiteCity.dat.gz",
+      src: 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz',
+      verbose: true)
+
+  gunzip(src: "${geoLiteSources}/GeoLiteCity.dat.gz")
+
+  delete(file: "${geoLiteSources}/GeoLiteCity.dat.gz")
+
+  updateConfig()
+
+  event('StatusFinal', ['GeoLite installed successfully'])
+}
+
+private void updateConfig() {
+  def configFile = new File(appDir, 'conf/Config.groovy')
+  if (configFile.exists()) {
+    configFile.withWriterAppend {
+      it.writeLine '\n// Added by the GeoIP plugin:'
+      it.writeLine "grails.plugin.geoip.data.resource = '${geoPath}/GeoLiteCity.dat'"
+    }
+  }
+}
+
+setDefaultTarget 'installGeoLite'
