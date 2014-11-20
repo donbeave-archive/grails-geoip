e9349a59867ee4d075a9cc2234cdc460769a7b51
Alexey Zhokhov <donbeave@gmail.com>
2014-11-20 11:57:03 +0800

    Upgraded Grails rest-client-builder plugin to 2.0.3 version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 
-//grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
     inherits('global') {
 
@@ -23,7 +23,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:1.0.3') {
+        build(':release:3.0.1', ':rest-client-builder:2.0.3') {
             export = false
         }
     }
